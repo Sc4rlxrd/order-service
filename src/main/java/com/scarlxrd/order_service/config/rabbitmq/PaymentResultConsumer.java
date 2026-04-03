26c275f3091b408cd1cff9d12bd279c1e3f2cebc
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-04-03 20:38:02 -0300

    feat(messaging): implement consumer for payment results

---
+++ src/main/java/com/scarlxrd/order_service/config/rabbitmq/PaymentResultConsumer.java
@@ -0,0 +1,31 @@
+package com.scarlxrd.order_service.config.rabbitmq;
+
+import com.scarlxrd.order_service.dto.PaymentResultEvent;
+import com.scarlxrd.order_service.entity.Order;
+import com.scarlxrd.order_service.entity.OrderStatus;
+import com.scarlxrd.order_service.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class PaymentResultConsumer {
+
+    private final OrderRepository repository;
+
+    @RabbitListener(queues = "payment.result.queue")
+    public void handlePaymentResult(PaymentResultEvent event) {
+
+        Order order = repository.findById(event.getOrderId())
+                .orElseThrow();
+
+        if ("SUCCESS".equals(event.getStatus())) {
+            order.setStatus(OrderStatus.PAID);
+        } else {
+            order.setStatus(OrderStatus.CANCELLED);
+        }
+
+        repository.save(order);
+    }
+}
