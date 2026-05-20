de948d62eb4a0ee584439b6996fcd17cc63e072d
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-05-19 22:19:48 -0300

    refactor: Adds pessimistic lock for concurrency.

--- src/main/java/com/scarlxrd/order_service/repository/OrderRepository.java
+++ src/main/java/com/scarlxrd/order_service/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package com.scarlxrd.order_service.repository;
 
 import com.scarlxrd.order_service.entity.Order;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +15,8 @@ public interface OrderRepository extends JpaRepository<Order, UUID> {
 
     @Query("SELECT o FROM Order o LEFT JOIN FETCH o.items WHERE o.id = :id")
     Optional<Order> findByIdWithItems(UUID id);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(" SELECT o FROM Order o LEFT JOIN FETCH o.items WHERE o.id = :id")
+    Optional<Order> findByIdForUpdate(UUID id);
 }
