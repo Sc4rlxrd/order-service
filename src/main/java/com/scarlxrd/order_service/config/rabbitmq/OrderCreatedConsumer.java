5b85be55b17e33f26ddb2f4b0b0fbe5ba9a0b8db
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-05-24 17:57:51 -0300

    feat: add order created consumer

---
+++ src/main/java/com/scarlxrd/order_service/config/rabbitmq/OrderCreatedConsumer.java
@@ -0,0 +1,46 @@
+package com.scarlxrd.order_service.config.rabbitmq;
+
+
+import com.scarlxrd.order_service.dto.OrderCreatedEvent;
+import com.scarlxrd.order_service.entity.Order;
+import com.scarlxrd.order_service.repository.OrderRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OrderCreatedConsumer {
+
+    private final OrderRepository repository;
+
+    @RabbitListener(
+            queues = "order.created.queue",
+            containerFactory = "rabbitListenerContainerFactory"
+    )
+    @Transactional
+    public void handle(OrderCreatedEvent event) {
+
+        log.info(
+                "Received order.created {}",
+                event.getOrderId()
+        );
+
+        Order order = repository.findById(
+                event.getOrderId()
+        ).orElseThrow();
+
+        order.getItems().forEach(item -> {
+            log.info(
+                    "Livro={} qtd={}",
+                    item.getBookId(),
+                    item.getQuantity()
+            );
+        });
+
+    }
+}
