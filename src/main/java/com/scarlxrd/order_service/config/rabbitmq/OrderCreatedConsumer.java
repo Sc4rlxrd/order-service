47dbae07f0e22241b55786bc12cceaffde954dc1
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-05-30 20:58:35 -0300

    chore: remove unused imports

--- src/main/java/com/scarlxrd/order_service/config/rabbitmq/OrderCreatedConsumer.java
+++ src/main/java/com/scarlxrd/order_service/config/rabbitmq/OrderCreatedConsumer.java
@@ -8,7 +8,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
